9caa6bbb143e6cedb8a3a62aa99bc2bd2e0d8ae8
Christine Greaney-Kelley <kelleyc416@gmail.com>
2021-08-28 16:18:48 +0100

    update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+SECRET_KEY = 'django-insecure-jlt4@40fr$p!o3wbh6#5s_&x%s#v^grg08@#3f!5_$q1b)exa9'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
